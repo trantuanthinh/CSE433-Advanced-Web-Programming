f0066d98effe567dea5b0fbe66edda9ac7b8450d
Trần Tuấn Thịnh <tran.tuan.thinh.0125@gmail.com>
2025-02-26 20:55:59 +0700

    commit

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,11 +1,44 @@
+import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import {fetchProductList} from "../../stimulate-api/stimulate-api";
+import {ProductType} from "../../types/ProductType";
+import ProductBlock from "../products/ProductBlock";
 
 export default function Search() {
     const {query} = useParams();
 
+    const [products, setProducts] = useState<ProductType[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchProductList();
+                const filteredProducts = data.filter((product) =>
+                    product.name.toLowerCase().includes(query?.toLowerCase() || "")
+                );
+                setProducts(filteredProducts);
+            } catch (error) {
+                console.error("Failed to fetch products:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [query]);
+
     return (
-        <div>
-            <h1>Search: {query}</h1>
-        </div>
+        <>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <section>
+                        <ProductBlock title={`Kết quả:  ${query}`} products={products} />
+                    </section>
+                </>
+            )}
+        </>
     );
 }
