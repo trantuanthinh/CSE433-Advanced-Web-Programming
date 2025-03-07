d3493b14a9b94580b753118b4920ff1197e4d224
Trần Tuấn Thịnh <tran.tuan.thinh.0125@gmail.com>
2025-03-07 11:16:50 +0700

    commit

--- src/components/cart/AddToCart.tsx
+++ src/components/cart/AddToCart.tsx
@@ -1,13 +1,19 @@
+import {toast} from "react-toastify";
 import {useMyContext} from "../../Context";
 import {ProductType} from "../../types/ProductType";
 
 export default function AddToCart(product: ProductType) {
     const {dispatch} = useMyContext();
 
+    const handleClick = () => {
+        dispatch({type: "ADD_TO_CART", product: product});
+        toast.success(`${product.name} added to cart.`);
+    };
+
     return (
         <button
             className="bg-blue-500 hover:bg-blue-700 text-white px-2 py-1 rounded"
-            onClick={() => dispatch({type: "ADD_TO_CART", product: product})}
+            onClick={handleClick}
         >
             Add to Cart
         </button>
